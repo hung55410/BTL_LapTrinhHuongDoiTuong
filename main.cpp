2bc8c0f552567a674646744869888478a3f8cbd5
H <82827774+stamping-machine-district-9@users.noreply.github.com>
2021-11-16 10:08:43 +0700

    main 2

--- main.cpp
+++ main.cpp
@@ -3,12 +3,654 @@
 #include <math.h>
 #include <windows.h>
 #include <conio.h>
-#include "ngayLeTrongThang.h"
+#include <string.h>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#include "ngayLe.h"
+//#include "giaodien.h"
 
 using namespace std;
 
+// Tạo cấu trúc Event
+struct Event
+{
+    string TieuDe;
+    int ngay, thang, nam;
+    int gio, phut;
+    string NoiDung;
+};
+
+// Tạo cấu trúc Node
+struct Node
+{
+    Event *data;
+    Node *pNext;
+};
+
+// Tạo cấu trúc danh sách LK đơn
+struct SingleList
+{
+    Node *pHead;
+};
+
+// Khởi tạo danh sách LK đơn
+void Initialize(SingleList *&list)
+{
+    list=new SingleList;
+    list->pHead=NULL;
+}
+
+// Cài đặt tháng
+void inputMonth(Event &ev) {
+    while (ev.thang < 1 || ev.thang > 12) {
+        cout << "Thang khong hop le." << endl;
+        cout << "Nhap lai thang: ";
+        cin >> ev.thang;
+    }
+    if(ev.thang == 1 || ev.thang == 3 || ev.thang == 5 || ev.thang == 7 || ev.thang == 8 || ev.thang == 10 || ev.thang == 12) {
+        cout << "Nhap ngay: ";
+        cin >> ev.ngay;
+        while (ev.ngay < 1 || ev.ngay > 31) {
+            cout << "Ngay khong hop le." << endl;
+            cout << "Nhap lai ngay: ";
+            cin >> ev.ngay;
+        }
+    } else if(ev.thang == 4 || ev.thang == 6 || ev.thang == 9 || ev.thang == 11) {
+        cout << "Nhap ngay: ";
+        cin >> ev.ngay;
+        while (ev.ngay < 1 || ev.ngay > 50) {
+            cout << "Ngay khong hop le." << endl;
+            cout << "Nhap lai ngay: ";
+            cin >> ev.ngay;
+        }
+    }
+}
+
+// Cài đặt năm
+void settingDay(Event &ev) {
+    cout << "Nhap nam: " ;
+    cin >> ev.nam;
+    if(ev.nam % 4 == 0 || ev.nam % 400 == 0) {
+        cout << "Nhap thang: ";
+        cin >> ev.thang;
+        if(ev.thang == 2) {
+            cout << "Nhap ngay: ";
+            cin >> ev.ngay;
+            do{
+                if(ev.ngay < 1 || ev.ngay > 29) {
+                    cout << "Nhap lai ngay: ";
+                    cin >> ev.ngay;
+                }
+            } while (ev.ngay < 1 || ev.ngay > 29);
+
+        } else {
+            inputMonth(ev);
+        }
+    } else {
+        cout << "Nhap thang: ";
+        cin >> ev.thang;
+        if(ev.thang == 2) {
+            cout << "Nhap ngay: ";
+            cin >> ev.ngay;
+            do{
+                if(ev.ngay < 1 || ev.ngay > 28) {
+                    cout << "Nhap lai ngay: ";
+                    cin >> ev.ngay;
+                }
+            } while (ev.ngay < 1 || ev.ngay > 28);
+        } else {
+            inputMonth(ev);
+        }
+    }
+}
+
+// Cài đặt giờ
+void settingHour(Event &ev) {
+    cout << "Nhap gio: ";
+    cin >> ev.gio;
+    do{
+        if(ev.gio < 0 || ev.gio > 23) {
+            cout << "Gio khong hop le." << endl;
+            cout << "Nhap lai gio: ";
+            cin >> ev.gio;
+        }
+    } while (ev.gio < 0 || ev.gio > 23);
+    do{
+        cout << "Nhap phut: ";
+        cin >> ev.phut;
+        if(ev.phut < 0 || ev.phut > 59) {
+            cout << "Phut khong hop le." << endl;
+            cout << "Nhap lai phut: ";
+            cin >> ev.phut;
+        }
+    } while (ev.phut < 0 || ev.phut > 59);
+}
+
+// Tạo một Node mới
+Node *CreateNode(Event *ev)
+{
+    Node *pNode=new Node;
+    if(pNode!=NULL)
+    {
+        pNode->data=ev;
+        pNode->pNext=NULL;
+    }
+    else
+    {
+        cout<<"cap phat bo nho that bai!!!";
+    }
+    return pNode;
+}
+
+// Thêm một sự kiện vào danh sách LK đơn
+Event *NhapEvent()
+{
+    Event *ev=new Event;
+    fflush(stdin);
+    cout << "Nhap tieu de: ";
+    getline(cin, ev->TieuDe);
+
+    settingDay(*ev);
+
+    settingHour(*ev);
+    
+    fflush(stdin);
+    cout << "Nhap noi dung: ";
+    getline(cin, ev->NoiDung);
+    return ev;
+}
+
+// Thêm một sự kiện vào cuối danh sách LK đơn
+void InsertLast(SingleList *&list,Event *ev)
+{
+    Node *pNode=CreateNode(ev);
+    if(list->pHead==NULL)
+    {
+        list->pHead=pNode;
+    }
+    else
+    {
+        Node *pTmp=list->pHead;
+         
+        while(pTmp->pNext!=NULL)
+        {
+            pTmp=pTmp->pNext;
+        }
+        pTmp->pNext=pNode;
+    }
+}
+
+// Hiển thị danh sách đã nhập
+void PrintList(SingleList *list)
+{
+    Node *pTmp=list->pHead;
+    if(pTmp==NULL)
+    {
+        cout<<"Danh sach rong" << endl;
+        return;
+    }
+    while(pTmp!=NULL)
+    {
+        Event *ev=pTmp->data;
+        cout << "__________________________________________________" << endl;
+        cout << endl;
+        cout << "Tieu de: " << ev->TieuDe << endl;
+        cout << "Thoi gian: " << ev->gio << ":" << ev->phut << ", " << ev->ngay << "/" << ev->thang << "/" << ev->nam << endl;
+        cout << "Noi dung: " << ev->NoiDung << endl;
+        cout << "__________________________________________________" << endl;
+        pTmp=pTmp->pNext;
+    }
+}
+
+// Hàm sắp xếp theo thời gian (sử dụng thuật toán sắp xếp trao đổi)
+void SortList(SingleList *&list)
+{
+    cout << "Sap xep theo ngay tang dan" << endl;
+    for(Node *p=list->pHead ; p!=NULL ; p=p->pNext)
+    {
+        for(Node *q=p->pNext ; q!=NULL ; q=q->pNext)
+        {
+            if(p->data->nam > q->data->nam){
+                Event *tmp=p->data;
+                p->data=q->data;
+                q->data=tmp;
+            }
+            else if(p->data->nam == q->data->nam)
+            {
+                if(p->data->thang > q->data->thang)
+                {
+                    Event *tmp=p->data;
+                    p->data=q->data;
+                    q->data=tmp;
+                }
+                else if(p->data->thang == q->data->thang)
+                {
+                    if(p->data->ngay > q->data->ngay)
+                    {
+                        Event *tmp=p->data;
+                        p->data=q->data;
+                        q->data=tmp;
+                    }
+                    else if(p->data->ngay == q->data->ngay)
+                    {
+                        if(p->data->gio > q->data->gio)
+                        {
+                            Event *tmp=p->data;
+                            p->data=q->data;
+                            q->data=tmp;
+                        }
+                        else if(p->data->gio == q->data->gio)
+                        {
+                            if(p->data->phut > q->data->phut)
+                            {
+                                Event *tmp=p->data;
+                                p->data=q->data;
+                                q->data=tmp;
+                            }
+                            else if(p->data->phut == q->data->phut)
+                            {
+                                if(p->data->gio > q->data->gio)
+                                {
+                                    Event *tmp=p->data;
+                                    p->data=q->data;
+                                    q->data=tmp;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Hàm sắp xếp theo thời gian (sử dụng thuật toán sắp xếp nổi bọt)
+void swap( Event *&a, Event *&b ){
+    Event *tmp=a;
+    a=b;
+    b=tmp;
+}
+void SortList1(SingleList *&list) {
+    Node *pTmp = list->pHead;
+    if(pTmp == NULL) {
+        cout << "Danh sach rong" << endl;
+        return;
+    }
+    while(pTmp->pNext != NULL) {
+        for(Node *q = pTmp->pNext ; q != NULL ; q = q->pNext) {
+            if(pTmp->data->nam > q->data->nam) {
+                swap(pTmp->data, q->data);
+            }
+            else if(pTmp->data->nam == q->data->nam) {
+                if(pTmp->data->thang > q->data->thang) {
+                    swap(pTmp->data, q->data);
+                }
+                else if(pTmp->data->thang == q->data->thang) {
+                    if(pTmp->data->ngay > q->data->ngay) {
+                        swap(pTmp->data, q->data);
+                    }
+                    else if(pTmp->data->ngay == q->data->ngay) {
+                        if(pTmp->data->gio > q->data->gio) {
+                            swap(pTmp->data, q->data);
+                        }
+                        else if(pTmp->data->gio == q->data->gio) {
+                            if(pTmp->data->phut > q->data->phut) {
+                                swap(pTmp->data, q->data);
+                            }
+                            else if(pTmp->data->phut == q->data->phut) {
+                                if(pTmp->data->gio > q->data->gio) {
+                                    swap(pTmp->data, q->data);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        pTmp = pTmp->pNext;
+    }
+}
+
+// Tìm kiếm theo tiêu đề (sử dụng thuật toán tìm kiếm nhị phân)
+void SearchByTitleEvent(SingleList *list)
+{
+    string title;
+    fflush(stdin);
+    cout << "Nhap tieu de can tim: ";
+    getline(cin, title);
+    Node *pTmp=list->pHead;
+    if(pTmp==NULL)
+    {
+        cout<<"Danh sach rong" << endl;
+        return;
+    }
+    while(pTmp!=NULL)
+    {
+        if(pTmp->data->TieuDe == title)
+        {
+            Event *ev=pTmp->data;
+            cout << "__________________________________________________" << endl;
+            cout << endl;
+            cout << "Tieu de: " << ev->TieuDe << endl;
+            cout << "Thoi gian: " << ev->gio << ":" << ev->phut << ", " << ev->ngay << "/" << ev->thang << "/" << ev->nam << endl;
+            cout << "Noi dung: " << ev->NoiDung << endl;
+            cout << "__________________________________________________" << endl;
+            break;
+        }
+        else{
+            cout << "Khong tim thay" << endl;
+        }
+        pTmp=pTmp->pNext;
+    }
+}
+
+// xoá theo tiêu đề
+void DeleteByTitle(SingleList *&list)
+{
+    string title;
+    fflush(stdin);
+    cout << "Nhap tieu de can xoa: ";
+    getline(cin, title);
+
+    Node *pDel=list->pHead;
+    if(pDel==NULL)
+    {
+        cout<<"Danh sach rong!";
+    }
+    else
+    {
+        Node *pPre=NULL;
+        while(pDel!=NULL)
+        {
+            Event *ev=pDel->data;
+            if(ev->TieuDe==title) 
+                break;
+            pPre=pDel;
+            pDel=pDel->pNext;
+        }
+        if(pDel==NULL)
+        {
+            cout<<"khong tim thay!!!" << endl;
+        }
+        else
+        {
+            if(pDel==list->pHead)
+            {
+                list->pHead=list->pHead->pNext;
+                pDel->pNext=NULL;
+                delete pDel;
+                pDel=NULL;
+            }
+            else
+            {
+                pPre->pNext=pDel->pNext; 
+                pDel->pNext=NULL;
+                delete pDel;
+                pDel=NULL;
+            }
+        }
+    }
+}
+
+// Đọc file .txt
+int readFile(SingleList *&list)
+{
+    fstream f;
+	f.open("event.txt", ios::in);
+
+	string data;
+
+	string line;
+	while (!f.eof())
+	{
+		getline(f, line);
+        cout << line << endl;
+	}
+
+	f.close();
+
+	cout << data;
+}
+
+// ghi file .txt
+void writeFile(SingleList *list) {
+    FILE *f;
+    f = fopen("event.txt", "w");
+    Node *p = list->pHead;
+    fprintf(f, "Danh sach ca su kien da them\n");
+    while(p != NULL) {
+        fprintf(f, "________________________________________\n");
+        fprintf(f, "Tieu de: %s\n", p->data->TieuDe.c_str());
+        fprintf(f, "Thoi Gian: %d/%d/%d\n", p->data->ngay, p->data->thang, p->data->nam);
+        fprintf(f, "           %d:%d\n", p->data->gio, p->data->phut);
+        fprintf(f, "Noi Dung: %s\n", p->data->NoiDung.c_str());
+        fprintf(f, "________________________________________\n");
+        p = p->pNext;
+    }
+    fclose(f);
+}
+void listMenuEvent() {
+    
+   	SingleList *list;
+    Event *ev;
+    Initialize(list);
+    int cn=1;
+	int a=1;
+    
+    do {
+        system("cls");
+        gotoxy(50,10);
+		if (cn == 1)cout << "==>"; cout << "Them moi" << endl;
+        gotoxy(50,11);    
+        if (cn == 2)cout << "==>"; cout << "Hien thi" << endl;
+        gotoxy(50,12);
+        if (cn == 3)cout << "==>"; cout << "Sap xep" << endl;
+        gotoxy(50,13);
+        if (cn == 4)cout << "==>"; cout << "Tim kiem" << endl;
+        gotoxy(50,14);
+        if (cn == 5)cout << "==>"; cout << "Xoa" << endl;
+        gotoxy(50,15);
+        if (cn == 6)cout << "==>"; cout << "Lua" << endl;
+        gotoxy(50,16);
+        if (cn == 7)cout << "==>"; cout << "Da luu" << endl;
+        gotoxy(50,17);
+        //if (cn == 8)cout << "==>"; cout << "Tro ve" << endl;
+		cout << "Nhan ESC de tro ve menu chinh" << endl;
+        //cout << "Chon " << cn;
+        //cout << "case 3" << endl;
+
+
+
+        int t = _getch();
+        //cout << t;
+        if (t == 13)
+        {
+            // nhan enter
+            switch (cn)
+            {
+            case 1:
+                {   
+                    system("cls");
+                    Event *ev= NhapEvent();
+                    InsertLast(list,ev);
+                    cout << "Them moi thanh cong!!! Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+            case 2:
+                {
+                    system("cls");
+                    PrintList(list);
+                    cout << "Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+            case 3:
+                {
+                    system("cls");
+                    SortList(list);
+                    cout << "DS sau khi sap xep la: " << endl;
+                    PrintList(list);
+                    cout << "Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+            case 4:
+                {
+                    system("cls");
+                    SearchByTitleEvent(list);
+                    cout << "Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+            case 5:
+                {
+                    system("cls");
+                    DeleteByTitle(list);
+                    cout << "Xoa thanh cong!!! Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+            case 6:
+                {
+                    system("cls");
+                    writeFile(list);
+                    cout << "Ghi file thanh cong!!! Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+            case 7:
+                {
+                    system("cls");
+                    readFile(list);
+                    cout << "Doc file thanh cong!!! Nhan phim bat ky de tro ve!!!" << endl;
+                    getch();
+                    break;
+                }
+  
+            default:
+                {
+                    cout << "Nhap sai!!!" << endl;
+                    break;
+                }
+            
+            }
+        }
+        else if (t == 27)//nhan esc
+        {
+            a=0;
+        }
+        else if (t == 80)//nhan mui ten xuong
+        {
+            cn++;
+        }
+        else if (t == 72)//nhan mui ten len
+        {
+            cn--;
+        }
+         if (cn > 7)
+        {
+             
+             cn = 1;
+        }
+        else if (cn < 1)
+        {
+             
+             cn = 7;
+        }
+    }
+    while (a);
+    
+}
+
+
+
+
+
+void menuMain(DateTime time) {
+    
+    int cn=1;
+
+    
+    do {
+        system("cls");
+        gotoxy(50,15);
+		if (cn == 1)cout << "==>"; cout << "Xem ngay le" << endl;
+            
+        gotoxy(50,16);
+        if (cn == 2)cout << "==>"; cout << "Tao su kien" << endl;
+        gotoxy(50,17);
+		cout << "Nhan ESC de thoat " << endl;
+        
+
+
+        int t = _getch();
+        
+        if (t == 13)
+        {
+            // nhan enter
+            switch (cn)
+            {
+		            case 1:
+		        {
+		            system("cls");
+		            time.ListMenuNgayLe();
+		            break;
+		        }
+		        case 2:
+		        {
+		            system("cls");
+		            listMenuEvent();
+		            break;
+		        }
+		        default:
+		        {
+		            cout << "Lua chon khong hop le" << endl;
+		            break;
+		        }
+            
+            }
+        }
+        else if (t == 27)//nhan esc
+        {
+            exit(0);
+        }
+        else if (t == 80)//nhan mui ten xuong
+        {
+            cn++;
+        }
+        else if (t == 72)//nhan mui ten len
+        {
+            cn--;
+        }
+         if (cn > 2)
+        {
+             
+             cn = 1;
+        }
+        else if (cn < 1)
+        {
+             
+             cn = 2;
+        }
+    }
+    while (1);
+    
+}
+
+
+
 int main(){
+    SingleList *list;
+    Initialize(list);
     DateTime time;
-    time.menu();
+    Event *ev;
+	set_color( 0 <<4|2);
+	setFontSize(25);
+	system("title QUAN LY SU KIEN"); 
+    menuMain(time);
+
+
     return 0;
 }
